ece218b865b93e0eea60540913a2e327986caa50
Sai-shashank-2005 <saishashankpedapati@gmail.com>
2026-04-06 07:17:31 +0530

    AESEncryption code initialized

---
+++ network_security/AESEncryption.java
@@ -0,0 +1,26 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class AESEncryption {
+    public static void main(String[] args) throws Exception {
+
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        SecretKey secretKey = keyGenerator.generateKey();
+
+        Cipher cipher = Cipher.getInstance("AES");
+        String plaintext = "Hello, World!";
+
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
+        String encryptedText = Base64.getEncoder().encodeToString(encryptedBytes);
+        System.out.println("Encrypted Text: " + encryptedText);
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        String decryptedText = new String(decryptedBytes);
+        System.out.println("Decrypted Text: " + decryptedText);
+    }
+}
